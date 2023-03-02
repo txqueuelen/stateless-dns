831b04e777ba743a407460e0f44fd2318f4ad371
Juan Manuel "Kang" Perez <devel@kangmak.es>
2023-03-03 00:04:12 +0100

    first iteration without tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 set -e -o pipefail
 
-function die () {
-  echo "$@"
-  exit 1
-}
-
-[ -f /etc/pdns/pdns.conf ] || \
-  die "No configuration file found at /etc/pdns/pdns.conf. Please, use the standard location."
-
-[ $(grep -E '^launch=gsqlite3$' /etc/pdns/pdns.conf | wc -l) -eq 1 ] || \
-  die "This image is meant to be used with sqlite backend"
-
-DATABASE=$(grep -E '^gsqlite3-database=' /etc/pdns/pdns.conf | cut -d= -f2-)
-
-if ! [ -f "$DATABASE" ]; then
-  sqlite3 "$DATABASE" < /usr/local/share/pdns/schema.sqlite3.sql
+if ! [ -f "$DATABASE_PATH" ]; then
+  sqlite3 "$DATABASE_PATH" < /usr/local/share/pdns/schema.sqlite3.sql
 
   for ZONEFILE in "${ZONEDIR:=/zones}"/*; do
     if [ -f "$ZONEFILE" ]; then
