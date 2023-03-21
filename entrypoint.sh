78f36cd9a75958470785615529e9445a3f7fb3cd
Roberto Santalla <roobre@users.noreply.github.com>
2023-03-21 15:29:49 +0100

    chart: fixes found during e2e rework (#26)
    Co-authored-by: Juan Manuel "Kang" Perez <devel@kangmak.es>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e -o pipefail
 
-if ! [ -f "$DATABASE_PATH" ]; then
-  sqlite3 "$DATABASE_PATH" < /usr/local/share/pdns/schema.sqlite3.sql
+DATABASE_PATH=${DATABASE_PATH:=/data/db.sqlite}
 
-  for ZONEFILE in "${ZONEDIR:=/zones}"/*; do
-    if [ -f "$ZONEFILE" ]; then
-      ZONENAME=$(basename "$ZONEFILE")
-      pdnsutil load-zone $ZONENAME "$ZONEFILE"
-      pdnsutil set-meta $ZONENAME SOA-EDIT-API DEFAULT
-    fi
-  done
-fi
+rm "$DATABASE_PATH" 2> /dev/null || true
+sqlite3 "$DATABASE_PATH" < /usr/local/share/pdns/schema.sqlite3.sql
+
+for ZONEFILE in "${ZONEDIR:=/zones}"/*; do
+  if [ -f "$ZONEFILE" ]; then
+    ZONENAME=$(basename "$ZONEFILE")
+    pdnsutil load-zone $ZONENAME "$ZONEFILE"
+    pdnsutil set-meta $ZONENAME SOA-EDIT-API DEFAULT
+  fi
+done
 
 exec "$@"
